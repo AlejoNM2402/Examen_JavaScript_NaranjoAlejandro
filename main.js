17c948e8a633581e34081e52758ad69d8d9f061d
AlejonM2402 <alejonaranjom4@gmail.com>
2025-04-29 12:16:38 -0500

    feat: :rocket: sisas

--- main.js
+++ main.js
@@ -5,11 +5,8 @@ let tiempo = document.getElementById('time')
 let porciones = document.getElementById('pieces')
 let categoria = document.getElementById('categori')
 
-let dificultad = document.getElementById('dificult')
+let dificultad = document.getElementById('dificult').value;
 
-
-const recetaInfo = document.createElement('div')
-document.body.appendChild(recetaInfo)
 
 
 const save = document.getElementById('saved')
@@ -17,16 +14,30 @@ const save = document.getElementById('saved')
 
 
 
-function print() {
-    const finalName = nombre.value.toLowerCase();
-    const finalingre = ingredientes.value.toLowerCase();
-    const finalinstruct = instrucciones.value.toLowerCase();
-    const finaltime = tiempo.value.toLowerCase();
-    const finalpieces = porciones.value.toLowerCase();
-    const finalcate = categoria.value.toLowerCase();
+function print(){ 
+    const finalName = nombre.value;
+    const finalingre = ingredientes.value;
+    const finalinstruct = instrucciones.value;
+    const finaltime = tiempo.value;
+    const finalpieces = porciones.value;
+    const finalcate = categoria.value;
     const final = ('Nombre:', finalName, 'Ingredientes:', finalingre, 'Instrucciones:', finalinstruct, 'Tiempo:', finaltime, 'Porciones:', finalpieces, 'Categoria:', finalcate)
-    recetaInfo.innerHTML = 
-    <p> Receta: ${final}</p>
+    const recetaInfo = document.createElement('div')
+    recetaInfo.classList.add('receta-box')
+    // recetaInfo.style.border = '4px solid black'
+    // recetaInfo.style.width = '20%'
+
+    document.body.appendChild(recetaInfo)
+    recetaInfo.innerHTML = `
+    <p> Receta: ${finalName}</p>
+    <p> Ingredientes: ${finalingre}</p>
+    <p> Instrucciones: ${finalinstruct}</p>
+    <p> Tiempo: ${finaltime}</p>
+    <p> Porciones: ${finalpieces}</p>
+    <p> Categoria: ${finalcate}</p>
+    <p> Dificultad: ${dificultad}</p> 
+    
+    `
 
     console.log('Nombre:', finalName, 'Ingredientes:', finalingre, 'Instrucciones:', finalinstruct, 'Tiempo:', finaltime, 'Porciones:', finalpieces, 'Categoria:', finalcate)
 }
@@ -36,4 +47,5 @@ function print() {
 
 save.addEventListener('click', () => {
     print(); 
+    alert('Receta guardada exitosamente')
 })
